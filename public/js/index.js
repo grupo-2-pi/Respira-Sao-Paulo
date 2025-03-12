3cabae514a5b52a4038474d6da4d1f864d996a9c
l-furquim <lucas.furquim@sptech.school>
2025-03-12 17:26:32 -0300

    adiciona correcoes no tamanho e na responsividade

--- public/js/index.js
+++ public/js/index.js
@@ -1,11 +1,25 @@
-function openSidebar(){
-  var sidebar = sideBar;
+var sidebar = sideBar;
 
+function openSidebar(){
   if(sidebar.style.display === "flex"){
+    
     sidebar.style.display = "none";
   }else{
     sidebar.style.display = "flex";
-  }
-
-  console.log(sidebar); 
+  
+      // Adicionar o listener após um delay
+    setTimeout(() => {
+      document.addEventListener('click', clickOutsideHandler);
+    }, 50);
 }
+
+  console.log(sidebar);
+}
+
+var clickOutsideHandler = (event) => {
+  if (!sidebar.contains(event.target)) {
+    sidebar.style.display = "none";
+    // Remover o listener após fechar a siedbar
+    document.removeEventListener('click', clickOutsideHandler);
+  }
+};
