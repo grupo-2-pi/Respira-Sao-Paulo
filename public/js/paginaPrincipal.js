fd2c6832a5286e59ae91e5bcc9b6289ef2d0e43c
RicardoOliveiraN <ricardo.nicolau@sptech.school>
2025-03-30 21:12:26 -0300

    Pagina Inicial v5

--- public/js/paginaPrincipal.js
+++ public/js/paginaPrincipal.js
@@ -1,11 +1,9 @@
 function abrirFiltro(){
-    id_filtro_aplicado.style.display = "none";
     id_fundo_escolher_filtro.style.display = "flex";
 
 }
 
 function fecharFiltro(){
-    id_filtro_aplicado.style.display = "flex";
     id_fundo_escolher_filtro.style.display = "none";
 
 }
