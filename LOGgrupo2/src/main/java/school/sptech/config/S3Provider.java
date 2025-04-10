9b534f5d0ecabe200b3f883f26ac56bcec93ab98
l-furquim <lucas.furquim@sptech.school>
2025-04-10 13:39:01 -0300

    finalizacao da config  do s3 sdk e adicao das funcoes envolvendo o memso

---
+++ LOGgrupo2/src/main/java/school/sptech/config/S3Provider.java
@@ -0,0 +1,31 @@
+package school.sptech.config;
+
+import software.amazon.awssdk.auth.credentials.AwsCredentials;
+import software.amazon.awssdk.auth.credentials.AwsSessionCredentials;
+import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+
+public class S3Provider {
+
+    private final S3Client s3Client;
+    private final AwsCredentials credentials;
+
+
+    public S3Provider(){
+        this.credentials = AwsSessionCredentials.create(
+                System.getenv("AWS_ACESS_KEY_ID"),
+                System.getenv("AWS_SECRET_ACCESS_KEY"),
+                System.getenv("AWS_SESSION_TOKEN")
+        );
+        this.s3Client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .credentialsProvider(() -> credentials)
+                .build();
+    }
+
+    public S3Client getS3Client() {
+        return s3Client;
+    }
+}
+
