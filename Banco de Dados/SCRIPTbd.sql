149ec80cfd7052e928f6db5aac2b3c781bedabc7
l-furquim <lucas.furquim@sptech.school>
2025-04-16 11:30:25 -0300

    correcao do script e adicao na nova tabela

--- Banco de Dados/SCRIPTbd.sql
+++ Banco de Dados/SCRIPTbd.sql
@@ -3,60 +3,91 @@ CREATE DATABASE Respira;
 
 USE Respira;
 
-CREATE TABLE Municipios (
-    idMunicipios INT AUTO_INCREMENT PRIMARY KEY,
-    nomeMunicipio VARCHAR(45) NOT NULL
+CREATE TABLE Admin (
+    idAdmin INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(45),
+    email VARCHAR(45) UNIQUE,
+    senha VARCHAR(45)
+);
+
+
+CREATE TABLE FrotaCirculante (
+    iddadosCETESB INT AUTO_INCREMENT PRIMARY KEY,
+    municipio VARCHAR(100),
+    regiao VARCHAR(100),
+    automoveis INT,
+    comerciais_leves INT,
+    caminhoes INT,
+    onibus INT,
+    motos INT,
+    ano VARCHAR(45),
+    total INT
+);
+
+CREATE TABLE EmissaoVeicular(
+    idEmissaoVeicular INT AUTO_INCREMENT PRIMARY KEY,
+    tipoVeiculo VARCHAR(100),
+    ano INT,
+    valorCO DOUBLE,
+    valorHC DOUBLE,
+    valorNOX DOUBLE,
+    valorRCHO DOUBLE,
+    valorMP DOUBLE,
+    valorN2O DOUBLE
+);
+
+
+CREATE TABLE MortalidadeRespiratoria (
+    iddadosCOVISA INT AUTO_INCREMENT PRIMARY KEY,
+    mes_ano VARCHAR(45),
+    municipio VARCHAR(45),
+    valorTotal DOUBLE,
+    numeroInternacoes DOUBLE,
+    numeroObitos INT,
+    taxaMortalidade DOUBLE,
+    regiao VARCHAR(100)
 );
 
 CREATE TABLE Empresa (
     idEmpresa INT AUTO_INCREMENT PRIMARY KEY,
-    CNPJ CHAR(14) UNIQUE NOT NULL,
-    nomeFantasia VARCHAR(45) NOT NULL,
-    qtdFuncionarios INT NOT NULL,
-    dataContratacao DATE NOT NULL,
-    dataFimContrato DATE
+    nomeFantasia VARCHAR(45),
+    cnpj CHAR(14) UNIQUE,
+    nomeEmpresa VARCHAR(45),
+    quantidadeFuncionarios INT,
+    emailContato VARCHAR(100)
 );
 
-CREATE TABLE Funcionarios (
-    idFuncionarios INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45) NOT NULL,
-    cpf CHAR(11) UNIQUE NOT NULL,
-    email VARCHAR(45) UNIQUE,
-    cargo VARCHAR(45) NOT NULL,
-    senha CHAR(8) NOT NULL,
-    idEmpresa INT NOT NULL,
+CREATE TABLE Funcionario (
+    idFuncionario INT AUTO_INCREMENT PRIMARY KEY,
+    cpfFuncionario CHAR(11) UNIQUE,
+    nomeFuncionario VARCHAR(100),
+    emailFuncionario VARCHAR(100) UNIQUE,
+    senha VARCHAR(45),
+    validarSenha VARCHAR(45), 
+    cargoFuncionario VARCHAR(45),
+    idGerente INT,
+    idEmpresa INT,
+    isFirstLogin TINYINT,
+    FOREIGN KEY (idGerente) REFERENCES Funcionario(idFuncionario),
     FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
 );
 
-CREATE TABLE poluicaoTransporte (
-    idpoluicaoTransporte INT AUTO_INCREMENT PRIMARY KEY,
-    tipoTransporte VARCHAR(45) NOT NULL,
-    qtdTransporte INT NOT NULL,
-    tipoPoluicao VARCHAR(45) NOT NULL,
-    qtdPoluicao DOUBLE NOT NULL,
-    anoDado INT NOT NULL,
-    tipoCombustivel VARCHAR(45),
-    idMunicipio INT NOT NULL,
-    FOREIGN KEY (idMunicipio) REFERENCES Municipios(idMunicipios)
+
+CREATE TABLE LogRestrito (
+    idLogRestrito INT AUTO_INCREMENT PRIMARY KEY,
+    tipo VARCHAR(45),
+    descricao VARCHAR(255),
+    dataHora DATETIME
 );
 
-CREATE TABLE dadosSaude (
-    iddadosSaude INT AUTO_INCREMENT PRIMARY KEY,
-    ano INT NOT NULL,
-    mes VARCHAR(45) NOT NULL,
-    gastosTotais DOUBLE NOT NULL,
-    numeroInternacoes INT NOT NULL,
-    numeroObitos INT NOT NULL,
-    faixaIdade VARCHAR(45),
-    idMunicipio INT NOT NULL,
-    FOREIGN KEY (idMunicipio) REFERENCES Municipios(idMunicipios)
-);
 
-CREATE TABLE FeedbackUsuarios (
-    idFeedbackUsuarios INT AUTO_INCREMENT PRIMARY KEY,
-    nomeUsuario VARCHAR(45) NOT NULL,
-    dataFeedback DATE NOT NULL,
-    descricaoFeedback VARCHAR(100) NOT NULL,
-    tipoPoluente VARCHAR(45),
-    municipioFeedback VARCHAR(45) NOT NULL
+CREATE TABLE Notificacoes (
+    idNotificacoes INT AUTO_INCREMENT PRIMARY KEY,
+    dataHora DATETIME,
+    descricao VARCHAR(255),
+    tipo VARCHAR(45),
+    idFuncionario INT,
+    idEmpresa INT,
+    FOREIGN KEY (idFuncionario) REFERENCES Funcionario(idFuncionario),
+    FOREIGN KEY (idEmpresa) REFERENCES Empresa(idEmpresa)
 );
