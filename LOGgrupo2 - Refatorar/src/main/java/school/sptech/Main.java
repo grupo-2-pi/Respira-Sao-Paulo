ef90ef48c0df3595b1792db372a9d3c415421281
ShirleyFerr <shirley.lima@sptech.school>
2025-05-23 18:10:19 -0300

    arrumei a main

--- LOGgrupo2 - Refatorar/src/main/java/school/sptech/Main.java
+++ LOGgrupo2 - Refatorar/src/main/java/school/sptech/Main.java
@@ -23,7 +23,7 @@ public class Main {
     public static JDBCConfig jdbcConfig = new JDBCConfig();
     private final static JdbcTemplate jdbcTemplate = jdbcConfig.getConnection();
     private final static ExcelUtils excelUtils = new ExcelUtils();
-    private final static MortalidadeRespiratoriaService mortalidadeService = new MortalidadeRespiratoriaServi(logger, excelUtils, jdbcTemplate);
+    private final static MortalidadeRespiratoriaService mortalidadeService = new MortalidadeRespiratoriaService(logger, excelUtils, jdbcTemplate);
     private static final S3Client s3Client = new S3Provider().getS3Client();
     private static final S3Service s3Service = new S3Service(s3Client, "respirasp-bucket", logger);
     private static final FrotaCirulanteService frotaCirculante = new FrotaCirulanteService(logger, excelUtils, jdbcTemplate);
