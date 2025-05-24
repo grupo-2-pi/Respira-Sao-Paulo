fd5c921517269169485c52e037c47d2a9eadc2f0
rafael <rafael.fernandes@sptech.school>
2025-05-24 18:08:52 -0300

    att

---
+++ site/src/models/dadosDashboardModel.js
@@ -0,0 +1,31 @@
+import { executar } from "../database/config.js";
+
+export async function getDadosFrotaCirculante(regiao, ano, mes) {
+    const query = `
+        SELECT municipio, automoveis, comerciais_leves, caminhoes, onibus, motos
+        FROM FrotaCirculante
+        WHERE regiao = '${regiao}' AND ano = '${ano}' AND mes = '${mes}';
+    `;
+    const resultado = await executar(query);
+    return resultado;
+}
+
+export async function getDadosMortalidade(regiao, ano, mes) {
+    const query = `
+        SELECT municipio, valorTotal, numeroInternacoes, numeroObitos, taxaMortalidade
+        FROM MortalidadeRespiratoria
+        WHERE regiao = '${regiao}' AND ano = '${ano}' AND mes = '${mes}';
+    `;
+    const resultado = await executar(query);
+    return resultado;
+}
+
+export async function getDadosQualidadeAr(regiao, ano, mes) {
+    const query = `
+        SELECT municipio, poluente, valor, unidade
+        FROM QualidadeAr
+        WHERE regiao = '${regiao}' AND ano = '${ano}' AND mes = '${mes}';
+    `;
+    const resultado = await executar(query);
+    return resultado;
+}
