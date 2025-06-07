4ea644f16c27d731122f90901bf519999a7cd10f
l-furquim <lucas.furquim@sptech.school>
2025-06-07 12:49:43 -0300

    corrige o get env que escrevi errado

--- LOGgrupo2/src/main/java/school/sptech/config/JDBCConfig.java
+++ LOGgrupo2/src/main/java/school/sptech/config/JDBCConfig.java
@@ -9,8 +9,8 @@ public class JDBCConfig {
 
     private final DataSource dataSource;
     private static final String host = "mysql";
-    private static final String user = System.getEnv("DB_USER");
-    private static final String password = System.getEnv("DB_PASSWORD");
+    private static final String user = System.getenv("DB_USER");
+    private static final String password = System.getenv("DB_PASSWORD");
 
     public JDBCConfig() {
         DriverManagerDataSource driver = new DriverManagerDataSource();
